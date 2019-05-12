b8427d24e2b64ac2e564346dd4b945d8ad815373
nlessmann <773597+nlessmann@users.noreply.github.com>
2019-05-12 14:27:24 +0200

    Adjusted license also in package setup file

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setuptools.setup(
     name='MIDL website builder',
     version=mwb.__version__,
     description='Tool for building static websites for the MIDL conference series',
-    url='https://midl.io',
-    license='CC-BY-SA-4.0',
+    url='https://www.midl.io',
+    license='Apache 2.0',
     author='Nikolas Lessmann',
     packages=['mwb'],
     install_requires=[
