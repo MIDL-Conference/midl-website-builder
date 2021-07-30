8d548d42a228562548bc16add2fcb3c9f4e74963
nlessmann <773597+nlessmann@users.noreply.github.com>
2021-07-30 13:54:04 +0200

    Make builder check whether the theme is missing

--- mwb/builder.py
+++ mwb/builder.py
@@ -119,6 +119,11 @@ class WebsiteBuilder:
             print(message, flush=True)
 
     def build(self, dstdir, clean=False):
+        # Check that theme is there
+        if len(glob(path.join(self.theme, '*'))) == 0:
+            self.print('Theme is missing, did you clone the repository with --recurse-submodules?')
+            return
+
         # Clear output directory
         self.print(f'Preparing output directory {dstdir}')
         if clean and path.exists(dstdir):
